1f857306a2e3166c6d01f742f82fa58516183ee6
Aditya Priyam <apriyam9@gmail.com>
2019-10-10 04:38:14 +0530

    User model created

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose= require('mongoose');
+
+const schema = mongoose.Schema;
+
+// Create a scheme for the User model
+const UserSchema= new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default: Date.now
+    },
+});
+
+module.exports = User = mongoose.model('users',UserSchema);
